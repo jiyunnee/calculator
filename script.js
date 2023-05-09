258718395323291472440990141c508740d83200
jiyunnee <130388257+jiyunnee@users.noreply.github.com>
2023-05-09 10:29:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,133 +1,21 @@
-const display = document.querySelector('.calculator-display');
+let result = document.getElementById("result");
 
-const numberButtons = document.querySelectorAll('[data-number]');
-
-const operatorButtons = document.querySelectorAll('[data-operator]');
-
-const clearButton = document.querySelector('[data-clear]');
-
-const equalsButton = document.querySelector('[data-equals]');
-
- 
-
-let currentValue = '';
-
-let previousValue = '';
-
-let currentOperator = null;
-
-let shouldResetDisplay = false;
-
- 
-
-numberButtons.forEach(button => {
-
-button.addEventListener('click', () => {
-
-if (shouldResetDisplay) {
-
-display.value = '';
-
-shouldResetDisplay = false;
-
+function appendNumber(number) {
+  result.value += number;
 }
 
-display.value += button.textContent;
-
-});
-
-});
-
- 
-
-operatorButtons.forEach(button => {
-
-button.addEventListener('click', () => {
-
-if (currentOperator) calculate();
-
-previousValue = display.value;
-
-currentOperator = button.dataset.operatorType;
-
-shouldResetDisplay = true;
-
-});
-
-});
-
- 
-
-equalsButton.addEventListener('click', () => {
-
-if (!currentOperator) return;
-
-calculate();
-
-});
-
- 
-
-clearButton.addEventListener('click', () => {
-
-currentValue = '';
-
-previousValue = '';
-
-currentOperator = null;
-
-display.value = '';
-
-});
-
- 
-
-function calculate() {
-
-currentValue = display.value;
-
-let result;
-
- 
-
-switch (currentOperator) {
-
-case 'add':
-
-result = parseFloat(previousValue) + parseFloat(currentValue);
-
-break;
-
-case 'subtract':
-
-result = parseFloat(previousValue) - parseFloat(currentValue);
-
-break;
-
-case 'multiply':
-
-result = parseFloat(previousValue) * parseFloat(currentValue);
-
-break;
-
-case 'divide':
-
-result = parseFloat(previousValue) / parseFloat(currentValue);
-
-break;
-
-default:
-
-return;
-
+function appendOperator(operator) {
+  result.value += operator;
 }
 
- 
+function calculateResult() {
+  try {
+    result.value = eval(result.value);
+  } catch (error) {
+    result.value = "Error";
+  }
+}
 
-display.value = result;
-
-currentOperator = null;
-
-shouldResetDisplay = true;
-
+function clearResult() {
+  result.value = "";
 }
